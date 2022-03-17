0cf27307c92e4b7b9ea870131b43ffb051c1574e
Gerald Olumide <lucignation@gmail.com>
2022-03-17 08:57:43 +0100

    README.md updated

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
   const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.lki96.mongodb.net/${process.env.MONGO_DATABASE}`;
 
   try {
-    // await mongoose.connect(MONGODB_URI);
-    await mongoose.connect(LOCAL_DB);
+    await mongoose.connect(MONGODB_URI);
+    // await mongoose.connect(LOCAL_DB);
     console.log('MongoDB connect');
   } catch (error) {
     console.log(error);
